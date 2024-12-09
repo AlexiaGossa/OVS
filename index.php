f200872ede46a8539514f88319e7d71954b67504
AlexiaGossa <102846702+AlexiaGossa@users.noreply.github.com>
2024-12-10 00:56:13 +0100

    New content

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ include BASE_DIR."/check/check-ping.php";
 include BASE_DIR."/check/cache-curl.php";
 include BASE_DIR."/check/check-url.php";
 include BASE_DIR."/check/check-cert.php";
+include BASE_DIR."/check/check-dns.php";
+include BASE_DIR."/check/check-raw.php";
 
 //echo "dir = ".__DIR__;
 //phpinfo();
@@ -17,10 +19,12 @@ print_r ( CheckCert ( "https://www.php.net/manual/en/function.usort.php" ) );
 print_r ( CheckCert ( "https://www.google.fr" ) );
 print_r ( CheckCert ( "https://www.google.com" ) );
 print_r ( CheckCert ( "https://jquery.com" ) );
-print_r ( CheckCert ( "https://redhat.com" ) );
-//$aData = CheckCert ( "https://10.138.45.10/" );
+*/
+
+//print_r ( $aData );
+/*
+
 
 //$aData = CheckCert ( "https://code.jquery.com/jquery-3.7.1.min.js" );
-print_r ( $aData );
+
 */
-//echo "error = ".$iReturn;
